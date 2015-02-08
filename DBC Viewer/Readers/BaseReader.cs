447b083b68218bab6ceb0b59a1ffa0c9b780eabc
41782992 <41782992@qq.com>
2015-02-08 10:21:43 +0800

    - 临时将class DBReaderFactory放在BaseReader.cs文件
    Signed-off-by: 41782992 <41782992@qq.com>

--- DBC Viewer/Readers/BaseReader.cs
+++ DBC Viewer/Readers/BaseReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace DBCViewer
@@ -12,5 +13,34 @@ namespace DBCViewer
         Dictionary<int, string> StringTable { get; }
         byte[] GetRowAsByteArray(int row);
         BinaryReader this[int row] { get; }
+    }
+
+    class DBReaderFactory
+    {
+        public static BaseReader GetReader(string file)
+        {
+            BaseReader reader = null;
+
+            var ext = Path.GetExtension(file).ToUpperInvariant();
+            switch (ext)
+            {
+                case ".DBC":
+                    reader = new DBCReader(file);
+                    break;
+                case ".DB2":
+                    reader = new DB2Reader(file);
+                    break;
+                case ".ADB":
+                    reader = new ADBReader(file);
+                    break;
+                case ".WDB":
+                    reader = new WDBReader(file);
+                    break;
+                default:
+                    throw new InvalidDataException(String.Format("Unknown file type {0}", ext));
+            }
+
+            return reader;
+        }
     }
 }
